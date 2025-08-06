53b4e117b719bca40946d5b98ed78ebc5aa1e1b8
satish2802 <satishchaurasia17e@gmail.com>
2025-08-06 20:21:46 +0530

    Update wsgi.py

--- elysieaesthetics/elysieaesthetics/wsgi.py
+++ elysieaesthetics/elysieaesthetics/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skincare.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elysieaesthetics.settings')
 
 application = get_wsgi_application()
+
