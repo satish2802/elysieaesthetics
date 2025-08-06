35097998c70002ac0e4ba81c7d98e46030e940a0
satish2802 <satishchaurasia17e@gmail.com>
2025-08-06 20:26:15 +0530

    Update asgi.py

--- elysieaesthetics/elysieaesthetics/asgi.py
+++ elysieaesthetics/elysieaesthetics/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skincare.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elysieaesthetics.settings')
 
 application = get_asgi_application()
+
