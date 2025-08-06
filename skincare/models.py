c8ddf2aaa68f9a3544152bf53ab8f2e5c165d340
satish2802 <satishchaurasia17e@gmail.com>
2025-08-06 16:05:38 +0530

    Delete skincare directory

--- skincare/models.py
+++
@@ -1,9 +0,0 @@
-# models.py
-from django.db import models
-
-class Appointment(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    date = models.CharField(max_length=100)
-    time = models.CharField(max_length=100)
-    service = models.CharField(max_length=100)
